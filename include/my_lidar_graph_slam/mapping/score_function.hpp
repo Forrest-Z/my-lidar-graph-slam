424fb5f2d1bb639593c8b37213b1193bbf409e71
Stern Gerlach <std.experimental.optional@gmail.com>
2020-04-26 00:34:08 +0900

    declare ScoreFunction class with template parameters

--- include/my_lidar_graph_slam/mapping/score_function.hpp
+++ include/my_lidar_graph_slam/mapping/score_function.hpp
@@ -5,19 +5,17 @@
 #define MY_LIDAR_GRAPH_SLAM_MAPPING_SCORE_FUNCTION_HPP
 
 #include "my_lidar_graph_slam/pose.hpp"
-#include "my_lidar_graph_slam/grid_map/binary_bayes_grid_cell.hpp"
-#include "my_lidar_graph_slam/grid_map/grid_map.hpp"
-#include "my_lidar_graph_slam/sensor/sensor_data.hpp"
 
 namespace MyLidarGraphSlam {
 namespace Mapping {
 
+template <typename T, typename U>
 class ScoreFunction
 {
 public:
     /* Type definitions */
-    using GridMapType = GridMap<BinaryBayesGridCell<double>>;
-    using ScanPtr = Sensor::ScanDataPtr<double>;
+    using GridMapType = T;
+    using ScanPtr = U;
 
     /*
      * Summary struct holds the details of the result
