e648276cff8b8eec322e089ccdada0f41f7ccf71
Stern Gerlach <std.experimental.optional@gmail.com>
2020-04-29 05:24:22 +0900

    do not use templates in ScoreFunction class

--- include/my_lidar_graph_slam/mapping/score_function.hpp
+++ include/my_lidar_graph_slam/mapping/score_function.hpp
@@ -4,19 +4,25 @@
 #ifndef MY_LIDAR_GRAPH_SLAM_MAPPING_SCORE_FUNCTION_HPP
 #define MY_LIDAR_GRAPH_SLAM_MAPPING_SCORE_FUNCTION_HPP
 
+#include <cmath>
+#include <cstdlib>
+#include <memory>
+
+#include "my_lidar_graph_slam/point.hpp"
 #include "my_lidar_graph_slam/pose.hpp"
+#include "my_lidar_graph_slam/grid_map/grid_map_base.hpp"
+#include "my_lidar_graph_slam/sensor/sensor_data.hpp"
 
 namespace MyLidarGraphSlam {
 namespace Mapping {
 
-template <typename T, typename U>
+/* Declare types for convenience */
+class ScoreFunction;
+using ScoreFuncPtr = std::shared_ptr<ScoreFunction>;
+
 class ScoreFunction
 {
 public:
-    /* Type definitions */
-    using GridMapType = T;
-    using ScanPtr = U;
-
     /*
      * Summary struct holds the details of the result
      */
@@ -46,8 +52,8 @@ public:
     ScoreFunction& operator=(ScoreFunction&&) = delete;
 
     /* Evaluate score function (matching score between scan data and map) */
-    virtual void Score(const GridMapType& gridMap,
-                       const ScanPtr& scanData,
+    virtual void Score(const GridMapBase<double>& gridMap,
+                       const Sensor::ScanDataPtr<double>& scanData,
                        const RobotPose2D<double>& sensorPose,
                        Summary& resultSummary) = 0;
 };
