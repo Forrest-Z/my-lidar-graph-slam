416ef4b368bb1f94cf0943a8050f803d54664318
Stern Gerlach <std.experimental.optional@gmail.com>
2020-04-29 04:55:40 +0900

    do not use templates in ScanMatcher class

--- include/my_lidar_graph_slam/mapping/scan_matcher.hpp
+++ include/my_lidar_graph_slam/mapping/scan_matcher.hpp
@@ -5,39 +5,48 @@
 #define MY_LIDAR_GRAPH_SLAM_MAPPING_SCAN_MATCHER_HPP
 
 #include <cmath>
+#include <cstdlib>
+#include <memory>
 
 #include <Eigen/Core>
 
 #include "my_lidar_graph_slam/pose.hpp"
 #include "my_lidar_graph_slam/util.hpp"
+#include "my_lidar_graph_slam/grid_map/grid_map_base.hpp"
+#include "my_lidar_graph_slam/sensor/sensor_data.hpp"
 
 namespace MyLidarGraphSlam {
 namespace Mapping {
 
-template <typename T, typename U>
 class ScanMatcher
 {
 public:
-    /* Type definitions */
-    using GridMapType = T;
-    using ScanType = U;
-
     /* Constructor */
     ScanMatcher() = default;
     /* Destructor */
     virtual ~ScanMatcher() = default;
 
+    /* Copy constructor (disabled) */
+    ScanMatcher(const ScanMatcher&) = delete;
+    /* Copy assignment operator (disabled) */
+    ScanMatcher& operator=(const ScanMatcher&) = delete;
+    /* Move constructor (disabled) */
+    ScanMatcher(ScanMatcher&&) = delete;
+    /* Move assignment operator (disabled) */
+    ScanMatcher& operator=(ScanMatcher&&) = delete;
+
     /* Optimize the robot pose by scan matching */
-    virtual void OptimizePose(const GridMapType& gridMap,
-                              const ScanType& scanData,
-                              const RobotPose2D<double>& initialPose,
-                              RobotPose2D<double>& estimatedPose,
-                              double& normalizedCostValue) = 0;
+    virtual void OptimizePose(
+        const GridMapBase<double>& gridMap,
+        const Sensor::ScanDataPtr<double>& scanData,
+        const RobotPose2D<double>& initialPose,
+        RobotPose2D<double>& estimatedPose,
+        double& normalizedCostValue) = 0;
     
     /* Calculate a covariance matrix */
     virtual void ComputeCovariance(
-        const GridMapType& gridMap,
-        const ScanType& scanData,
+        const GridMapBase<double>& gridMap,
+        const Sensor::ScanDataPtr<double>& scanData,
         const RobotPose2D<double>& robotPose,
         Eigen::Matrix3d& estimatedCovMat) = 0;
 };
