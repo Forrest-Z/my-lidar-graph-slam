507e562946c1f39d7e02e71849346256413566aa
Stern Gerlach <std.experimental.optional@gmail.com>
2020-06-02 17:09:07 +0900

    Create ScanAccumulator class to compute accumulated scan data

---
+++ include/my_lidar_graph_slam/mapping/scan_accumulator.hpp
@@ -0,0 +1,46 @@
+
+/* scan_accumulator.hpp */
+
+#ifndef MY_LIDAR_GRAPH_SLAM_MAPPING_SCAN_ACCUMULATOR_HPP
+#define MY_LIDAR_GRAPH_SLAM_MAPPING_SCAN_ACCUMULATOR_HPP
+
+#include <deque>
+#include <vector>
+
+#include "my_lidar_graph_slam/point.hpp"
+#include "my_lidar_graph_slam/pose.hpp"
+#include "my_lidar_graph_slam/util.hpp"
+#include "my_lidar_graph_slam/sensor/sensor_data.hpp"
+
+namespace MyLidarGraphSlam {
+namespace Mapping {
+
+class ScanAccumulator final
+{
+public:
+    /* Type definitions */
+    using ScanDataDeque = std::deque<Sensor::ScanDataPtr<double>>;
+
+    /* Constructor */
+    ScanAccumulator(std::size_t numOfAccumulatedScans);
+
+    /* Destructor */
+    ~ScanAccumulator() = default;
+
+    /* Append the scan data */
+    void AppendScan(const Sensor::ScanDataPtr<double>& scanData);
+
+    /* Retrieve the concatenated scan data  */
+    Sensor::ScanDataPtr<double> ComputeConcatenatedScan();
+
+private:
+    /* Number of the scan data to be accumulated */
+    std::size_t   mNumOfAccumulatedScans;
+    /* Accumulated scans */
+    ScanDataDeque mAccumulatedScans;
+};
+
+} /* namespace Mapping */
+} /* namespace MyLidarGraphSlam */
+
+#endif /* MY_LIDAR_GRAPH_SLAM_MAPPING_SCAN_ACCUMULATOR_HPP */
