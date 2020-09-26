1b365eb321fa38ef4247ebb28e308047d4060422
Stern Gerlach <std.experimental.optional@gmail.com>
2020-09-26 13:05:32 +0900

    Create LidarGraphSlamFrontend class that serves as a Graph-based SLAM frontend

---
+++ include/my_lidar_graph_slam/mapping/lidar_graph_slam_frontend.hpp
@@ -0,0 +1,87 @@
+
+/* lidar_graph_slam_frontend.hpp */
+
+#ifndef MY_LIDAR_GRAPH_SLAM_MAPPING_LIDAR_GRAPH_SLAM_FRONTEND_HPP
+#define MY_LIDAR_GRAPH_SLAM_MAPPING_LIDAR_GRAPH_SLAM_FRONTEND_HPP
+
+#include <memory>
+
+#include "my_lidar_graph_slam/point.hpp"
+#include "my_lidar_graph_slam/pose.hpp"
+#include "my_lidar_graph_slam/util.hpp"
+#include "my_lidar_graph_slam/mapping/lidar_graph_slam.hpp"
+#include "my_lidar_graph_slam/mapping/scan_interpolator.hpp"
+#include "my_lidar_graph_slam/mapping/scan_matcher.hpp"
+#include "my_lidar_graph_slam/sensor/sensor_data.hpp"
+
+namespace MyLidarGraphSlam {
+namespace Mapping {
+
+class LidarGraphSlamFrontend
+{
+public:
+    /* Type declarations for convenience */
+    using GridMapType = LidarGraphSlam::GridMapType;
+
+    /* Constructor */
+    LidarGraphSlamFrontend(
+        const ScanInterpolatorPtr& scanInterpolator,
+        const ScanMatcherPtr& scanMatcher,
+        const RobotPose2D<double>& initialPose,
+        const double updateThresholdTravelDist,
+        const double updateThresholdAngle,
+        const double updateThresholdTime,
+        const int loopClosureInterval);
+    /* Destructor */
+    ~LidarGraphSlamFrontend() = default;
+
+    /* Copy constructor (disabled) */
+    LidarGraphSlamFrontend(const LidarGraphSlamFrontend&) = delete;
+    /* Copy assignment operator (disabled) */
+    LidarGraphSlamFrontend& operator=(const LidarGraphSlamFrontend&) = delete;
+    /* Move constructor */
+    LidarGraphSlamFrontend(LidarGraphSlamFrontend&&) = default;
+    /* Move assignment operator */
+    LidarGraphSlamFrontend& operator=(LidarGraphSlamFrontend&&) = default;
+
+    /* Process scan data and odometry information */
+    bool ProcessScan(LidarGraphSlam* const pParent,
+                     const Sensor::ScanDataPtr<double>& rawScanData,
+                     const RobotPose2D<double>& odomPose);
+
+    /* Retrieve the total number of the processed input data */
+    inline int ProcessCount() const { return this->mProcessCount; }
+
+private:
+    /* The total number of the processed input data */
+    int                   mProcessCount;
+    /* Scan interpolator */
+    ScanInterpolatorPtr   mScanInterpolator;
+    /* Scan matcher */
+    ScanMatcherPtr        mScanMatcher;
+    /* Initial pose */
+    RobotPose2D<double>   mInitialPose;
+    /* Last odometry pose */
+    RobotPose2D<double>   mLastOdomPose;
+    /* Accumulated travel distance since the last map update */
+    double                mAccumulatedTravelDist;
+    /* Accumulated angle since the last map update */
+    double                mAccumulatedAngle;
+    /* Odometry pose at the last map update */
+    RobotPose2D<double>   mLastMapUpdateOdomPose;
+    /* Time of the last map update */
+    double                mLastMapUpdateTime;
+    /* Map update threshold for accumulated travel distance */
+    double                mUpdateThresholdTravelDist;
+    /* Map update threshold for accumulated angle */
+    double                mUpdateThresholdAngle;
+    /* Map update threshold for the elapsed time since the last map update */
+    double                mUpdateThresholdTime;
+    /* Frame interval for performing loop closure */
+    int                   mLoopClosureInterval;
+};
+
+} /* namespace Mapping */
+} /* namespace MyLidarGraphSlam */
+
+#endif /* MY_LIDAR_GRAPH_SLAM_MAPPING_LIDAR_GRAPH_SLAM_FRONTEND_HPP */
---
+++ src/my_lidar_graph_slam/mapping/lidar_graph_slam_frontend.cpp
@@ -0,0 +1,131 @@
+
+/* lidar_graph_slam_frontend.cpp */
+
+#include "my_lidar_graph_slam/mapping/lidar_graph_slam_frontend.hpp"
+
+namespace MyLidarGraphSlam {
+namespace Mapping {
+
+/* Constructor */
+LidarGraphSlamFrontend::LidarGraphSlamFrontend(
+    const ScanInterpolatorPtr& scanInterpolator,
+    const ScanMatcherPtr& scanMatcher,
+    const RobotPose2D<double>& initialPose,
+    const double updateThresholdTravelDist,
+    const double updateThresholdAngle,
+    const double updateThresholdTime,
+    const int loopClosureInterval) :
+    mProcessCount(0),
+    mScanInterpolator(scanInterpolator),
+    mScanMatcher(scanMatcher),
+    mInitialPose(initialPose),
+    mLastOdomPose(0.0, 0.0, 0.0),
+    mAccumulatedTravelDist(0.0),
+    mAccumulatedAngle(0.0),
+    mLastMapUpdateOdomPose(0.0, 0.0, 0.0),
+    mLastMapUpdateTime(0.0),
+    mUpdateThresholdTravelDist(updateThresholdTravelDist),
+    mUpdateThresholdAngle(updateThresholdAngle),
+    mUpdateThresholdTime(updateThresholdTime),
+    mLoopClosureInterval(loopClosureInterval)
+{
+}
+
+/* Process scan data and odometry information */
+bool LidarGraphSlamFrontend::ProcessScan(
+    LidarGraphSlam* const pParent,
+    const Sensor::ScanDataPtr<double>& rawScanData,
+    const RobotPose2D<double>& odomPose)
+{
+    /* Interpolate scan data if necessary */
+    auto scanData = (this->mScanInterpolator != nullptr) ?
+        this->mScanInterpolator->Interpolate(rawScanData) : rawScanData;
+
+    /* Calculate the relative odometry pose */
+    const RobotPose2D<double> relOdomPose = (this->mProcessCount == 0) ?
+        RobotPose2D<double>(0.0, 0.0, 0.0) :
+        InverseCompound(this->mLastOdomPose, odomPose);
+
+    this->mLastOdomPose = odomPose;
+
+    /* Accumulate the linear and angular distance */
+    this->mAccumulatedTravelDist += Distance(relOdomPose);
+    this->mAccumulatedAngle += std::fabs(relOdomPose.mTheta);
+
+    /* Compute the elapsed time since the last map update */
+    const double elapsedTime = (this->mProcessCount == 0) ?
+        0.0 : scanData->TimeStamp() - this->mLastMapUpdateTime;
+
+    /* Map is updated only when the robot moved more than the specified
+     * distance or the specified time has elapsed since the last map update */
+    const bool travelDistThreshold =
+        this->mAccumulatedTravelDist >= this->mUpdateThresholdTravelDist;
+    const bool angleThreshold =
+        this->mAccumulatedAngle >= this->mUpdateThresholdAngle;
+    const bool timeThreshold =
+        elapsedTime >= this->mUpdateThresholdTime;
+    const bool isFirstScan = this->mProcessCount == 0;
+    const bool mapUpdateNeeded =
+        travelDistThreshold || angleThreshold ||
+        timeThreshold || isFirstScan;
+
+    if (!mapUpdateNeeded)
+        return false;
+
+    /* Update the pose graph */
+    if (this->mProcessCount == 0) {
+        /* Set the initial pose for the first scan */
+        RobotPose2D<double> estimatedPose = this->mInitialPose;
+        /* Append the new pose graph node */
+        pParent->AppendNode(estimatedPose, scanData);
+    } else {
+        /* Retrieve the latest pose and the latest map */
+        RobotPose2D<double> latestPose;
+        GridMapType latestMap;
+        pParent->GetLatestPoseAndMap(latestPose, latestMap);
+
+        const RobotPose2D<double> relPoseFromLastMapUpdate =
+            InverseCompound(this->mLastMapUpdateOdomPose, odomPose);
+        const RobotPose2D<double> initialPose =
+            Compound(latestPose, relPoseFromLastMapUpdate);
+
+        /* Perform scan matching against the grid map that contains
+         * the latest scans and obtain the result */
+        ScanMatcher::Summary scanMatchSummary;
+        this->mScanMatcher->OptimizePose(
+            latestMap, scanData, initialPose, scanMatchSummary);
+
+        const RobotPose2D<double>& estimatedPose =
+            scanMatchSummary.mEstimatedPose;
+        const Eigen::Matrix3d& estimatedCovariance =
+            scanMatchSummary.mEstimatedCovariance;
+
+        /* Append a new pose graph node and odometry edge */
+        pParent->AppendOdometryNodeAndEdge(
+            estimatedPose, estimatedCovariance, scanData);
+    }
+
+    /* Integrate the scan data into the grid map and
+     * check if the new local map is created */
+    const bool localMapCreated = pParent->UpdateGridMap();
+
+    /* Notify the worker thread for the SLAM backend
+     * if the new local map is added to the entire grid map */
+    if (this->mProcessCount > this->mLoopClosureInterval &&
+        this->mProcessCount % this->mLoopClosureInterval == 0)
+        pParent->NotifyBackend();
+
+    /* Update miscellaneous parameters */
+    this->mProcessCount += 1;
+    this->mAccumulatedTravelDist = 0.0;
+    this->mAccumulatedAngle = 0.0;
+    this->mLastMapUpdateOdomPose = odomPose;
+    this->mLastMapUpdateTime = scanData->TimeStamp();
+
+    std::cerr << "Processing frame: " << this->mProcessCount << std::endl;
+
+    return true;
+}
+
+} /* namespace Mapping */
+} /* namespace MyLidarGraphSlam */
