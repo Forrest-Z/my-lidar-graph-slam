2b1fc618006865c9454283e1605ef03e17af7604
Stern Gerlach <std.experimental.optional@gmail.com>
2020-10-04 00:33:40 +0900

    Add more methods to ScanNodeMap class

--- include/my_lidar_graph_slam/mapping/pose_graph.hpp
+++ include/my_lidar_graph_slam/mapping/pose_graph.hpp
@@ -265,6 +265,46 @@ public:
     inline const MapType& Nodes() const
     { return this->mNodes; }
 
+    /* Get an iterator to the first scan node */
+    inline MapType::iterator begin() noexcept
+    { return this->mNodes.begin(); }
+    /* Get an iterator to the first scan node */
+    inline MapType::const_iterator begin() const noexcept
+    { return this->mNodes.begin(); }
+    /* Get an iterator to the first scan node */
+    inline MapType::const_iterator cbegin() const noexcept
+    { return this->mNodes.cbegin(); }
+
+    /* Get an iterator to the end scan node */
+    inline MapType::iterator end() noexcept
+    { return this->mNodes.end(); }
+    /* Get an iterator to the end scan node */
+    inline MapType::const_iterator end() const noexcept
+    { return this->mNodes.end(); }
+    /* Get an iterator to the end scan node */
+    inline MapType::const_iterator cend() const noexcept
+    { return this->mNodes.cend(); }
+
+    /* Get an iterator to the first scan node of the reversed map */
+    inline MapType::reverse_iterator rbegin() noexcept
+    { return this->mNodes.rbegin(); }
+    /* Get an iterator to the first scan node of the reversed map */
+    inline MapType::const_reverse_iterator rbegin() const noexcept
+    { return this->mNodes.rbegin(); }
+    /* Get an iterator to the first scan node of the reversed map */
+    inline MapType::const_reverse_iterator crbegin() const noexcept
+    { return this->mNodes.crbegin(); }
+
+    /* Get an iterator to the end scan node of the reversed map */
+    inline MapType::reverse_iterator rend() noexcept
+    { return this->mNodes.rend(); }
+    /* Get an iterator to the end scan node of the reversed map */
+    inline MapType::const_reverse_iterator rend() const noexcept
+    { return this->mNodes.rend(); }
+    /* Get an iterator to the end scan node of the reversed map */
+    inline MapType::const_reverse_iterator crend() const noexcept
+    { return this->mNodes.crend(); }
+
     /* Get the iterator pointing to the first scan node whose Id is
      * greater than the specified Id */
     inline MapType::const_iterator UpperBound(const NodeId nodeId) const
@@ -273,6 +313,13 @@ public:
      * greater than or equal to the specified Id */
     inline MapType::const_iterator LowerBound(const NodeId nodeId) const
     { return this->mNodes.lower_bound(nodeId); }
+
+    /* Check if the scan nodes are empty */
+    inline bool Empty() const
+    { return this->mNodes.empty(); }
+    /* Get the size of the scan nodes */
+    inline std::size_t Size() const
+    { return this->mNodes.size(); }
 
     /* Check if the scan node with the specified Id exists */
     inline bool Contains(const NodeId nodeId) const
