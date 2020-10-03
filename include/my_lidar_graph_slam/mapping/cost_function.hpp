6dc4f359b3268ed604d44ad5adf2e11f1f756986
Stern Gerlach <std.experimental.optional@gmail.com>
2020-10-04 03:46:00 +0900

    Use a sensor pose in a map-local coordinate frame in CostFunction methods

--- include/my_lidar_graph_slam/mapping/cost_function.hpp
+++ include/my_lidar_graph_slam/mapping/cost_function.hpp
@@ -40,21 +40,22 @@ public:
     CostFunction& operator=(CostFunction&&) = delete;
 
     /* Calculate cost function (mismatch between scan data and map) */
-    virtual double Cost(const GridMapBase<double>& gridMap,
-                        const Sensor::ScanDataPtr<double>& scanData,
-                        const RobotPose2D<double>& sensorPose) = 0;
-    
-    /* Calculate a gradient vector */
+    virtual double Cost(
+        const GridMapBase<double>& gridMap,
+        const Sensor::ScanDataPtr<double>& scanData,
+        const RobotPose2D<double>& mapLocalSensorPose) = 0;
+
+    /* Calculate a gradient vector in a map-local coordinate frame */
     virtual Eigen::Vector3d ComputeGradient(
         const GridMapBase<double>& gridMap,
         const Sensor::ScanDataPtr<double>& scanData,
-        const RobotPose2D<double>& sensorPose) = 0;
+        const RobotPose2D<double>& mapLocalSensorPose) = 0;
 
-    /* Calculate a covariance matrix */
+    /* Calculate a covariance matrix in a map-local coordinate frame */
     virtual Eigen::Matrix3d ComputeCovariance(
         const GridMapBase<double>& gridMap,
         const Sensor::ScanDataPtr<double>& scanData,
-        const RobotPose2D<double>& sensorPose) = 0;
+        const RobotPose2D<double>& mapLocalSensorPose) = 0;
 };
 
 } /* namespace Mapping */
